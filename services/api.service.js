11e8eabf6b20667d4814b6765d6f0cfab43026b9
Виктор Лобынцев <sibfight@mail.ru>
2025-01-02 23:05:56 +0300

    поправил старт

--- services/api.service.js
+++ services/api.service.js
@@ -1,4 +1,4 @@
-import axios, { Axios, AxiosError } from 'axios';
+import axios from 'axios';
 import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 
 export const getIcon = (icon) => {
